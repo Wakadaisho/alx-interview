00cdf644b95f3daa300a40aba1a096078aa17cd9
Wakadaisho <paulwahome9@gmail.com>
2023-12-13 09:21:23 +0300

    Change approach to recursion

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,20 +11,7 @@ def minOperations(n) -> int:
     needed to result in exactly n H characters in the file
     using two operations on a file starting with 1 H
     """
-    from math import log, floor, sqrt
-
-    if (n <= 1):
-        return 0
-
-    # Find first divisor
-    divisor: int = n
-    max: int = floor(sqrt(n)) + 1
-
-    for i in range(2, max):
+    for i in range(2, n):
         if n % i == 0:
-            divisor = i
-            break
-    if divisor == n:
-        return n
-
-    return floor(log(n, divisor) * divisor)
+            return i + minOperations(n // i)
+    return n > 1 and n or 0
